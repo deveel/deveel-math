a8972b401ac030abc52ba05b1c8e80e8f530d65a
Antonello Provenzano <antonello@deveel.com>
2014-04-02 01:05:35 +0200

    Structure refactoring: solution converted to VS2013 format (MSBuild); NuGet references and restore Code fixes: messages clash removed; assembly manifest restored Deployment: added NuGet specifications

--- src/Deveel.Math/Properties/AssemblyInfo.cs
+++ src/Deveel.Math/Properties/AssemblyInfo.cs
@@ -5,19 +5,19 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("dmath")]
+[assembly: AssemblyTitle("Deveel.Math")]
 [assembly: AssemblyDescription("Deveel Math Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Deveel")]
 [assembly: AssemblyProduct("Deveel.Math")]
-[assembly: AssemblyCopyright("Copyright © Deveel 2009-2012")]
+[assembly: AssemblyCopyright("(c) 2010-2014 Deveel")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2b62749d-d066-4df4-8f1f-002a43ba9fe8")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.2.0")]
-[assembly: AssemblyFileVersion("0.9.2.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
