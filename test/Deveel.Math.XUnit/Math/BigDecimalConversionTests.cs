99b4c1f6026718dd3f36054489ff33d522f25cc0
Antonello Provenzano <antonello@deveel.com>
2024-07-07 19:01:15 +0200

    Merge from the master branch

--- test/Deveel.Math.XUnit/Math/BigDecimalConversionTests.cs
+++ test/Deveel.Math.XUnit/Math/BigDecimalConversionTests.cs
@@ -7,30 +7,31 @@ namespace Deveel.Math
     public static class BigDecimalConversionTests
     {
         [Theory]
-        [InlineData("1")]
-        [InlineData("-1")]
-        public static void ConvertToBoolean_ShouldReturnTrue(string value)
+        [InlineData("1", true)]
+        [InlineData("0", false)]
+        public static void ConvertToBoolean_ShouldReturn(string value, bool expected)
         {
             var bigDecimal = BigDecimal.Parse(value);
-            var result = Convert.ToBoolean(bigDecimal);
-            Assert.True(result);
+            var result = Convert.ToBoolean((object) bigDecimal);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("-1")]
+        [InlineData("3.1")]
+        [InlineData("56600490011.1345")]
+        public static void ConvertToBoolean_ShouldThrowInvalidCastException(string value)
+        {
+            var bigDecimal = BigDecimal.Parse(value);
+            Assert.Throws<InvalidCastException>(() => Convert.ToBoolean((object) bigDecimal));
         }
 
         [Fact]
         public static void ConvertZeroToBoolean_ShouldReturnFalse()
         {
             var bigDecimal = BigDecimal.Zero;
-            var result = Convert.ToBoolean(bigDecimal);
+            var result = Convert.ToBoolean((object) bigDecimal);
             Assert.False(result);
-        }
-
-        [Theory]
-        [InlineData("0.1")]
-        [InlineData("-0.1")]
-        public static void ConvertFractionToBoolean_ShouldThrow(string value)
-        {
-            var bigDecimal = BigDecimal.Parse(value);
-            Assert.Throws<ArithmeticException>(() => Convert.ToBoolean(bigDecimal));
         }
     }
 }
